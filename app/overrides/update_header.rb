053adfeed4486eba4d55a1ee6a8094760b9f87c1
CARLOS GOMEZ <carlosgomez.deb@gmail.com>
2016-07-21 17:20:02 -0500

    adding elements to spree view

---
+++ app/overrides/update_header.rb
@@ -0,0 +1,8 @@
+Deface::Override.new(:virtual_path => 'spree/shared/_header',
+    :name => "adding elements to header",
+    :insert_before => "div#spree-header",
+    :text => "
+      <div class='shipping-free text-center'>
+        <p>Envió gratis en México a partir de $1,500.00</p>
+      </div>
+    ")
---
+++ app/overrides/update_main_nav_bar.rb
@@ -0,0 +1,3 @@
+Deface::Override.new(:virtual_path => 'spree/shared/_main_nav_bar',
+    :name => "removing elements from the main navbar",
+    :remove => "div#main-nav-bar")
