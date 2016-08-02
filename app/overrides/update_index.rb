4a8a66d62d714955b7c8d5153ebbea7db046431a
CARLOS GOMEZ <carlosgomez.deb@gmail.com>
2016-08-01 20:34:11 -0500

    multiple changes to frontend views

--- app/overrides/update_index.rb
+++ app/overrides/update_index.rb
@@ -1,8 +1,7 @@
-Deface::Override.new(:virtual_path  => 'spree/home/index',
-        name: 'adding background',
-        insert_before: '[data-hook="homepage_products"]',
-        text: "
-          <div class='landing-background'>
-            <h2>Autenticos sombreros Panama tejidos en Ecuador</h2>
-          </div>
-        ")
+# Deface::Override.new(:virtual_path  => 'spree/layouts/spree_application',
+#         name: 'adding background',
+#         insert_after: '#spree-header',
+#         text: "
+#           <div class='landing-background'>
+#           </div>
+#         ")
--- app/overrides/update_layout.rb
+++ app/overrides/update_layout.rb
@@ -1,43 +1,137 @@
+# Head
 Deface::Override.new(:virtual_path  => 'spree/layouts/spree_application',
-        name: 'adding footer',
-        insert_after: 'div.container',
+        name: 'Custom layout HEAD',
+        replace: "[data-hook='inside_head']",
         text: "
-        <div id='footer' class='container'>
-          <div class='col-md-4'>
-            <h3>Ubicación</h3>
-            <p>Reforma '#'222, Ciudad de México</p>
-            <p>C.P. 50000</p>
-            <p>Telefonos: (555) 123 5678</p>
-          </div>
-          <div class='col-md-4'>
-            <h3>Conoce mas de PH</h3>
-            <p>Nuestra historia</p>
-            <p>Nuestro equipo</p>
-          </div>
-          <div class='col-md-4'>
-            <h3>Redes Sociales</h3>
-            <p>Facebook</p>
-            <p>Twitter</p>
-            <p>Pinterest</p>
-            <p>Instagram</p>
-          </div>
-          <div class='col-md-12 text-center'>
-            <p>Derechos Reservados 2016 | Panama Hats Inc.</p>
+          <%= render partial: 'spree/shared/head' %>
+        ")
+# Body
+Deface::Override.new(:virtual_path  => 'spree/layouts/spree_application',
+        name: 'Custom layout BODY',
+        replace: "[data-hook='body']",
+        text: "
+        <!-- Header  -->
+        <div class='shipping-free text-center'>
+        <p>Envió gratis en México a partir de $1,500.00</p>
+        </div>
+        <%= render partial: 'spree/shared/google_analytics.js' %>
+        <%= render partial: 'spree/shared/header' %>
+
+        <!-- Heder bg -->
+        <% if current_page?(root_path) %>
+        <div class='landing-background'>
+        </div>
+        <% end %>
+
+        <div class='container'>
+          <div class='row' data-hook>
+            <%= breadcrumbs(@taxon) %>
+
+            <%= render partial: 'spree/shared/sidebar' if content_for? :sidebar %>
+
+            <div id='content' class='<%= !content_for?(:sidebar) ? 'col-sm-12' : 'col-sm-8 col-md-9' %>' data-hook>
+              <%= flash_messages %>
+              <%= yield %>
+            </div>
+
+            <%= yield :templates %>
           </div>
         </div>
+
+        <!-- Subscribe -->
+        <% if current_page?(root_path) %>
+          <section id='subscribe'>
+            <div class='content-subscribe'>
+              <p>Suscríbete para recibir lo último de Panama Hats Inc. </p>
+              <form id='subscription_form' class='subscription_form'>
+                <input id='subscribe_email' placeholder='Tu email aquí' type='text'></input>
+                <input class='submit_subscribe' value='Suscríbirse' type='submit'></input>
+              </form>
+            </div>
+          </section>
+
+        <!-- Footer -->
+          <div id='footer' class='container'>
+            <div class='col-md-4'>
+              <h3>Ubicación</h3>
+              <p>Reforma #222, Ciudad de México</p>
+              <p>C.P. 50000</p>
+              <p>Telefonos: (555) 123 5678</p>
+            </div>
+            <div class='col-md-4'>
+              <h3>Conoce mas de PH</h3>
+              <p>Nuestra historia</p>
+              <p>Nuestro equipo</p>
+              <h3>Buscar Ayuda</h3>
+              <p>Ayuda</p>
+              <p>Politica de devoluciones</p>
+              <p>Envío</p>
+              <p>Pagos</p>
+            </div>
+            <div class='col-md-4'>
+              <h3>Redes Sociales</h3>
+              <p><%= link_to 'Facebook', 'https://facebook.com' %></p>
+              <p><%= link_to 'Twitter', 'https://twitter.com' %></p>
+              <p><%= link_to 'Pinterest', 'https://pinterest.com' %></p>
+              <p><%= link_to 'Instagram', 'https://instagram.com' %></p>
+            </div>
+          </div>
+          <div class='col-md-12 copy'>
+            <ul class='float-rigth footer_ul mid_with text-center'>
+              <li>Trabaja con Nosotros</li>
+              <li>Terminos y condiciones</li>
+              <li>Politica de privacidad</li>
+            </ul>
+            <p class='mid_with text-center'> &copy; Derechos Reservados 2016 | Panama Hats Inc.</p>
+          </div>
+        <% end %>
         ")
 
-Deface::Override.new(:virtual_path  => 'spree/layouts/spree_application',
-        name: 'adding subscribe',
-        insert_before: 'div#footer',
-        text: "
-        <section id='subscribe'>
-          <div class='content-subscribe'>
-            <p>Suscríbete para recibir lo último de Panama Hats Inc. </p>
-            <form id='subscription_form' class='subscription_form'>
-              <input id='subscribe_email' placeholder='Tu email aquí' type='text'></input>
-              <input class='submit_subscribe' value='suscríbirse' type='submit'></input>
-            </form>
-          </div>
-        </section>
-        ")
+
+# Deface::Override.new(:virtual_path  => 'spree/layouts/spree_application',
+#         name: 'adding footer',
+#         insert_after: 'div.container',
+#         text: "
+#         <% if current_page?(root_path) %>
+#           <div id='footer' class='container'>
+#             <div class='col-md-4'>
+#               <h3>Ubicación</h3>
+#               <p>Reforma #222, Ciudad de México</p>
+#               <p>C.P. 50000</p>
+#               <p>Telefonos: (555) 123 5678</p>
+#             </div>
+#             <div class='col-md-4'>
+#               <h3>Conoce mas de PH</h3>
+#               <p>Nuestra historia</p>
+#               <p>Nuestro equipo</p>
+#             </div>
+#             <div class='col-md-4'>
+#               <h3>Redes Sociales</h3>
+#               <p><%= link_to 'Facebook', 'https://facebook.com' %></p>
+#               <p><%= link_to 'Twitter', 'https://twitter.com' %></p>
+#               <p><%= link_to 'Pinterest', 'https://pinterest.com' %></p>
+#               <p><%= link_to 'Instagram', 'https://instagram.com' %></p>
+#             </div>
+#           </div>
+#           <div class='col-md-12 text-center copy'>
+#             <p> &copy; Derechos Reservados 2016 | Panama Hats Inc.</p>
+#           </div>
+#         <% end %>
+#         ")
+#
+# Deface::Override.new(:virtual_path  => 'spree/layouts/spree_application',
+#         name: 'adding subscribe',
+#         insert_before: 'div#footer',
+#         text: "
+#         <% if current_page?(root_path) %>
+#           <section id='subscribe'>
+#             <div class='content-subscribe'>
+#               <p>Suscríbete para recibir lo último de Panama Hats Inc. </p>
+#               <form id='subscription_form' class='subscription_form'>
+#                 <input id='subscribe_email' placeholder='Tu email aquí' type='text'></input>
+#                 <input class='submit_subscribe' value='Suscríbirse' type='submit'></input>
+#               </form>
+#             </div>
+#           </section>
+#         <% end %>
+#         ")
